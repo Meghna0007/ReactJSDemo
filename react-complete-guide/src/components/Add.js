1a379fdcf4a7a17137b90aece745d4df8dffb1d0
Meghna <megnajaiswal007@gmail.com>
2020-10-01 21:47:24 +0530

    AddingReactProject

---
+++ react-complete-guide/src/components/Add.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class Add extends React.Component {
+    render(){
+        return(
+            <div>
+                <div className="container">
+<img src="assets/images/onlinePic.png" style-width="100%" alt="Online Pic" />
+<button className="btn">View More</button>
+</div>
+
+<p>This demo online store is powered by X-Cart.</p>
+<p>This service is for demo purpose only.</p>
+
+            </div>)
+    }
+}
+            export default Add
---
+++ react-complete-guide/src/components/Table1.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+class Table1 extends React.Component {
+    render(){
+        return(
+            <div>
+                <h3><b> Bestseller</b> </h3>
+                <table style={{width:"100%"}} border="0px" solid="#ebebe0" frame="box" cellpadding="0" cellspacing="0" border-collapse="collapse">
+<tr>
+<td style={{borderRight:"4px"}} solid="#ebebe0">
+  <img src="assets/images/product2.png" alt="T-shirt" style={{width:"100%"}} />
+</td>
+
+<td style={{borderRight:"1px"}} solid=" #ebebe0">
+  <img src="assets/images/product1.png" alt="Yoda Plush Backpack" style={{width:"100%"}} />
+</td>
+
+<td style={{borderRight:"1px"}} solid="#ebebe0">
+  <img src="assets/images/product4.png" alt="Pyramid Brain Twist" style={{width:"100%"}} />
+</td>
+
+<td style={{borderRight:"1px"}} solid ="#ebebe0">
+  <img src="assets/images/product5.png" alt="Bare Metal Bender" style={{width:"100%"}} />
+</td>
+
+<td style={{borderRight:"1px"}} solid ="#ebebe0">
+  <img src="assets/images/product7.png" alt="Albert Einstein Action Figure" style={{width:"100%"}} />
+</td>
+
+<td>
+  <img src="assets/images/product9.png" alt="Mini Solar-Powered Car" style={{width:"100%"}} />
+</td>
+</tr>
+<tr>
+<td style={{borderRight:"1px"}}solid=" #ebebe0">
+  <p align left className ="card">T-shirt</p>
+  <p className="price">$19.99</p>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star"></span>
+  <span className="fa fa-star"></span>
+ <p>
+<i className="fa fa-shopping-cart" style={{color:"orange"}}float="right"margin-right="9px"></i></p></td>
+<td style={{borderRight:"1px"}} solid="#ebebe0">
+  <p align left className ="card">T-shirt</p>
+  <p className="price">$19.99</p>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star"></span>
+  <span className="fa fa-star"></span>
+ <p> 
+<i className="fa fa-shopping-cart" style={{color:"orange"}}float="right" margin-right="9px"></i></p></td>
+<td style={{borderRight:"1px"}} solid="#ebebe0">
+  <p align left className ="card">T-shirt</p>
+  <p className="price">$19.99</p>
+ <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star"></span>
+  <span className="fa fa-star"></span>
+ <p>
+<i className="fa fa-shopping-cart" style={{color:"orange"}}float="right" margin-right="9px"></i></p></td>
+
+<td style={{borderRight:"1px"}} solid ="#ebebe0">
+  <p align left className ="card">T-shirt</p>
+  <p className="price">$19.99</p>
+<span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star"></span>
+  <span className="fa fa-star"></span>
+ <p>
+<i className="fa fa-shopping-cart" style={{color:"orange"}}float="right" margin-right=
+"9px"></i></p></td>
+
+<td style-border-right= "1px" solid ="#ebebe0">
+  <p align left className ="card">T-shirt</p>
+  <p className="price">$19.99</p>
+<span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:" orange"}}></span>
+  <span className="fa fa-star"></span>
+  <span className="fa fa-star"></span>
+<p>
+<i className="fa fa-shopping-cart" style={{color:"orange"}}float="right" margin-right="9px"></i></p>
+</td>
+
+<td>
+  <p align left className ="card">T-shirt</p>
+  <p className="price">$19.99</p>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star checked" style={{color:"orange"}}></span>
+  <span className="fa fa-star"></span>
+  <span className="fa fa-star"></span>
+ <p> 
+  <i className="fa fa-shopping-cart" style={{color:"orange"}} float="right" margin-right="9px"></i>
+  </p></td></tr>
+
+</table>
+
+</div>)
+        }
+    }
+    export default Table1
